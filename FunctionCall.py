40e1cd7820d53bfff2fcdf885c2abdfd43b34c2e
Sathya Pramod Bhat Muniyooru Nataraja <sathya.nataraja@outlook.com>
2021-09-07 00:11:18 +0530

    Update and rename main to FunctionCall.py

---
+++ FunctionCall.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from utils import *
+
+df = pd.read_csv('heart.csv')
+
+df1 = df.copy()
+del df1['Target']
+
+KNN = RunKNN(df1, df['target'], 0.25, 40, 5)
+
+DT = RunDecisionTree(df1, df['target'], 0.25, 40,15)
+
+RF = RunRandomForest(df1, df['target'], 0.25, 40,5)
+
+GB = RunGradientBoostingClassifier(df1, df['target'], 0.25, 40)
+
+XGB = RunXGBOOST(df1, df['Status'], 0.33, 45)
+
+
