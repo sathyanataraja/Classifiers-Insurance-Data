29fba181e6351206fbf6713f6509a4abf2929b88
Sathya Pramod Bhat Muniyooru Nataraja <sathya.nataraja@outlook.com>
2021-09-07 00:07:32 +0530

    Update and rename utils to ClassifiersUtils.py

--- ClassifiersUtils.py
+++ ClassifiersUtils.py
@@ -1,9 +1,25 @@
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import GradientBoostingClassifier
+
+def RunKNN(TrainData, Target, TestSize, RandomState, neighbors):
+    """ The function splits the data, fits the XGBoost model, predits the classification and probablity of each classification and returns the tet set with the values in a dataframe"""
+    X_train, X_test, y_train, y_test = train_test_split(TrainData, Target, test_size=TestSize, random_state=RandomState)
+    clf = KNeighborsClassifier(n_neighbors = neighbors)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_prob = clf.predict_proba(X_test)
+    predictions = [round(value) for value in y_pred] 
+    X_test['StatusPrediction'] = y_pred
+    X_test['StatusProbability_0'] = y_prob[:,0]
+    X_test['StatusProbability_1'] = y_prob[:,1]
+    Accuracy = accuracy_score(y_test, predictions)
+    AccuracyPercent = (Accuracy * 100.0)
+    return X_test,AccuracyPercent
 
 def RunDecisionTree(TrainData, Target, TestSize, RandomState, MaxDepth):
     """ The function splits the data, fits the tree, predits the classification and probablity of each classification and returns the tet set with the values in a dataframe"""
